732c4cfd9496a1f50c7e874b2243caf4d595fe7e
iamyonjan <surendrayonjan@gmail.com>
2015-03-09 17:31:03 +0545

    First Reduce by job to count number of drug users per year with genders

--- build.sbt
+++ build.sbt
@@ -6,3 +6,5 @@ lazy val commonSettings = Seq(
 
 lazy val root = (project in file("."))
 					.settings(commonSettings: _*)
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "1.2.1"
--- src/main/scala/com/nw/tuts/App.scala
+++ src/main/scala/com/nw/tuts/App.scala
@@ -1,8 +1,24 @@
 package com.nw.tuts
 
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+
 object App {
 
 	def main(args: Array[String]) = {
-		Console.println("Hello World!")
+		
+		val logFile = "data/hard_drug_users_by_sex.csv"
+		val conf = new SparkConf().setAppName("Simple Tutorial")
+		val sc = new SparkContext(conf)
+
+		val csvData = sc.textFile(logFile, 2).cache()
+
+		val pairedData = csvData.map{ line => 
+								var lineArr = line.split(",")
+								//( (year, gender), count )
+								( (lineArr(0), lineArr(2)), lineArr(3).toInt )
+							}
+		pairedData.reduceByKey(_ + _).collect() foreach ( println(_) )
 	}
 }
